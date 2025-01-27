6851b01b05e8aeaf108b03bee990f8cc9242104e
rajjau <elf-watch-mug@duck.com>
2025-01-27 15:40:45 -0500

    Added lagged features; changed feature names to be more descriptive.

--- modules/add_features.py
+++ modules/add_features.py
@@ -6,6 +6,11 @@ from pandas import Timedelta,to_datetime
 ######################
 from modules.messages import msg_info
 
+################
+### SETTINGS ###
+################
+LAGGED_MAX_DAYS = 5
+
 #################
 ### FUNCTIONS ###
 #################
@@ -17,7 +22,7 @@ def open_to_close(data):
     # All close values in $data.
     data_close_values = data['c']
     # Define the name of the new column that describes whether the opening value for the symbol is less than, equal to, or greater than the closing value.
-    column = 'o_c'
+    column = 'close_greater_than_open'
     # All instances where the closing value is less than or equal to the opening value.
     data.loc[data_close_values <= data_open_values, column] = 0
     # All instances where the closing value is greater than the opening value.
@@ -33,43 +38,79 @@ def time(data):
     # Add the full date (YYYY-MM-DD).
     data['t_d'] = timestamps.dt.date
     # Add the day of week.
-    data['t_w'] = timestamps.dt.day_name()
+    data['day_of_week'] = timestamps.dt.day_name()
     # Add the previous day of the week name as well.
-    data['t_w_p'] = (timestamps - Timedelta(days = 1)).dt.day_name()
+    data['lagged_day_of_week'] = (timestamps - Timedelta(days = 1)).dt.day_name()
     # Return the $data.
     return(data)
 
-def previous_day(data):
-    # Display informational message to stdout.
-    msg_info('Feature: Adding values from the previous day.')
-    # Sort the DataFrame by the symbol name and timestamp. This groups stocks and ensures the data for a given stock is in order from earliest to most recent.
-    data = data.sort_values(by = ['T', 't'])
-    # Add the previous day's closing value.
-    data['c_p'] = data.groupby('T')['c'].shift(1)
-    # Add the previous day's highest price value.
-    data['h_p'] = data.groupby('T')['h'].shift(1)
-    # Add the previous day's lowest price value.
-    data['l_p'] = data.groupby('T')['l'].shift(1)
-    # Add the previous day's number of transactions.
-    data['n_p'] = data.groupby('T')['n'].shift(1)
-    # Add the previous day's opening value.
-    data['o_p'] = data.groupby('T')['o'].shift(1)
-    # Add the previous day's volume.
-    data['v_p'] = data.groupby('T')['v'].shift(1)
-    # Add the previous day's volume weighted averaged price.
-    data['vw_p'] = data.groupby('T')['vw'].shift(1)
-    # Return the $data.
-    return(data)
+class lagged:
+    def __init__(self, data):
+        # Display informational message to stdout.
+        msg_info('Feature: Adding features from previous days.')
+        # Sort the DataFrame by the symbol name and timestamp. This groups stocks and ensures the data for a given stock is in order from earliest to most recent.
+        data = data.sort_values(by = ['T', 't'])
+        # Add features from the previous days.
+        for previous_day in range(1, LAGGED_MAX_DAYS + 1):
+            # Add the previous day's closing value.
+            data = self.obtain_data_from_previous_day(data = data, column_name = 'c', new_column_name = f'lagged_close_{previous_day}', previous_day = previous_day)
+            # Add the previous day's highest price value.
+            data = self.obtain_data_from_previous_day(data = data, column_name = 'h', new_column_name = f'lagged_high_{previous_day}', previous_day = previous_day)
+            # Add the previous day's lowest price value.
+            data = self.obtain_data_from_previous_day(data = data, column_name = 'l', new_column_name = f'lagged_low_{previous_day}', previous_day = previous_day)
+            # Add the previous day's number of transactions.
+            data = self.obtain_data_from_previous_day(data = data, column_name = 'n', new_column_name = f'lagged_number_transactions_{previous_day}', previous_day = previous_day)
+            # Add the previous day's opening value.
+            data = self.obtain_data_from_previous_day(data = data, column_name = 'o', new_column_name = f'lagged_open_{previous_day}', previous_day = previous_day)
+            # Add the previous day's volume.
+            data = self.obtain_data_from_previous_day(data = data, column_name = 'v', new_column_name = f'lagged_volume_{previous_day}', previous_day = previous_day)
+            # Add the previous day's volume weighted averaged price.
+            data = self.obtain_data_from_previous_day(data = data, column_name = 'vw', new_column_name = f'lagged_volume_weighted_avg_{previous_day}', previous_day = previous_day) 
+        # Make $data an internal variable to the class.
+        self._data = data
+
+    @property
+    def data(self):
+        # Return the $data.
+        return(self._data)
+    
+    def obtain_data_from_previous_day(self, data, column_name, new_column_name, previous_day = 1):
+        # Obtain data from the specified previous day, ensuring to group by the symbol name. The previous day variable only pulls data from the day that occured X days ago, not everything up to it.
+        data[new_column_name] = data.groupby('T')[column_name].shift(previous_day)
+        # Return the $data.
+        return(data)
+
+class feature_engineering:
+    def __init__(self, data):
+        # Display informational message to stdout.
+        msg_info('Feature: Creating new features based off existing ones.')
+        # Sort the DataFrame by the symbol name and timestamp. This groups stocks and ensures the data for a given stock is in order from earliest to most recent.
+        data = data.sort_values(by = ['T', 't'])
+        # Add new features from the previous days.
+        for previous_day in range(1, LAGGED_MAX_DAYS + 1):
+            # Add the spread between the high and low prices.
+            data[f"lagged_high_low_spread_{previous_day}"] = data[f"lagged_high_{previous_day}"] - data[f"lagged_low_{previous_day}"]
+            # Add the return from the previous days.
+            data[f"lagged_daily_return_{previous_day}"] = (data[f"lagged_close_{previous_day}"] - data[f"lagged_open_{previous_day}"]) / data[f"lagged_open_{previous_day}"]
+        # Make $data an internal variable to the class.
+        self._data = data
+
+    @property
+    def data(self):
+        # Return the $data.
+        return(self._data)
 
 ############
 ### MAIN ###
 ############
 def main(data):
     # Add string labels that describe when the opening value is less, equal, or greater than the closing value. These will be converted to int using one-hot-encoding before machine learning.
-    data = open_to_close(data)
+    data = open_to_close(data = data)
     # Calculate the different aspects of time for each row based on the timestamp ('t') value.
-    data = time(data)
-    # Add the values from the previous day.
-    data = previous_day(data)
+    data = time(data = data)
+    # Add features from previous days.
+    data = lagged(data = data).data
+    # Create new features from the existing data.
+    data = feature_engineering(data = data).data
     # Return the modified $data.
     return(data)
