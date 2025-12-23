622fb9b3870b27564f9482ce1c6917c45c2db6cc
rajjau <elf-watch-mug@duck.com>
2025-12-22 19:37:48 -0500

    Added ability to choose normalization method using configuration INI

--- modules/train_test_split.py
+++ modules/train_test_split.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 from numpy import ravel,unique
 from pandas import DataFrame
-from sklearn.preprocessing import RobustScaler
+from sklearn.preprocessing import RobustScaler, StandardScaler, MinMaxScaler
+
+#############################
+### NORMALIZATION METHODS ###
+#############################
+NORMALIZE_METHODS = {
+    'MinMaxScaler': MinMaxScaler,
+    'RobustScaler': RobustScaler,
+    'StandardScaler': StandardScaler
+}
 
 #################
 ### FUNCTIONS ###
@@ -16,13 +25,13 @@ def holdout(data, days):
     # Return the training and test datasets.
     return(data_train, data_test)
 
-def split_X_y(data, columns_x, columns_y, normalize_X = True):
+def split_X_y(data, columns_x, columns_y, normalize_X, normalize_method):
     # Define the feature columns as a DataFrame.
     X = data[columns_x]
     # Check if the variable to standardize the data was set to bool True.
     if normalize_X is True:
         # Initialize the scaler.
-        scaler = RobustScaler()
+        scaler = NORMALIZE_METHODS[normalize_method]()
         # Fit and transform the features data.
         X = scaler.fit_transform(X)
         # Convert $X back into a DataFrame.
@@ -34,16 +43,15 @@ def split_X_y(data, columns_x, columns_y, normalize_X = True):
 
 ############
 ### MAIN ###
-############
-def main(data, columns_x, columns_y, holdout_days, normalize_X = True):
+def main(data, columns_x, columns_y, holdout_days, normalize_X, normalize_method):
     # Ensure the $data is sorted from past to present.
     data = data.sort_values(by = 't', ascending = True)
     # Define the last X days as a holdout. This will split the data into training and test datasets.
     [data_train, data_test] = holdout(data, days = holdout_days)
     # Split the training datasets into feature and label columns.
-    [X_train, y_train] = split_X_y(data = data_train, columns_x = columns_x, columns_y = columns_y)
+    [X_train, y_train] = split_X_y(data = data_train, columns_x = columns_x, columns_y = columns_y, normalize_X = normalize_X, normalize_method = normalize_method)
     # Split the test datasets into feature and label columns.
-    [X_test, y_test] = split_X_y(data = data_test, columns_x = columns_x, columns_y = columns_y, normalize_X = normalize_X)
+    [X_test, y_test] = split_X_y(data = data_test, columns_x = columns_x, columns_y = columns_y, normalize_X = normalize_X, normalize_method = normalize_method)
     # Return the training and testing data.
     return(X_train, X_test, y_train, y_test)
 
