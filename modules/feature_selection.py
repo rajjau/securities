81fc30bbe0ba6d805d70fb119382e165139ed740
rajjau <elf-watch-mug@duck.com>
2025-12-27 20:49:09 -0500

    Handles percentage changes that are now being used when adding features

--- modules/feature_selection.py
+++ modules/feature_selection.py
@@ -26,9 +26,8 @@ def apply_selected_features(X_train, X_test, selected_features):
 
 def variance_threshold(X, feature_names):
     """Perform VarianceThreshold feature selection to remove features with low variance."""
-    # Initialize the function. See:
-    # https://scikit-learn.org/stable/modules/generated/sklearn.feature_selection.VarianceThreshold.html
-    selector = VarianceThreshold(threshold = 0.1)
+    # Initialize the function. Threshold is lowered to 0.0001 to support stationary/percentage features.
+    selector = VarianceThreshold(threshold = 0.0001)
     # Fit and transform the training data.
     selector.fit_transform(X = X)
     # Define the selected features.
@@ -38,8 +37,7 @@ def variance_threshold(X, feature_names):
 
 def select_k_best(X, y, feature_names, k):
     """Perform SelectKBest feature selection to select features based on mutual information."""
-    # Initialize the function. See:
-    # https://scikit-learn.org/stable/modules/generated/sklearn.feature_selection.SelectKBest.html
+    # Initialize the function.
     selector = SelectKBest(mutual_info_classif, k = k)
     # Fit and transform the training data.
     selector.fit_transform(X = X, y = ravel(y))
@@ -52,10 +50,8 @@ def select_k_best(X, y, feature_names, k):
 
 def recursive_feature_elimination(X, y, feature_names):
     """Perform Recursive Feature Elimination with Cross-Validation (RFECV) to select features."""
-    # Initialize the function. See:
-    # https://scikit-learn.org/stable/modules/generated/sklearn.feature_selection.RFECV.html
+    # Initialize the function.
     selector = RFECV(estimator = DecisionTreeClassifier(), cv = TimeSeriesSplit(n_splits = 5), n_jobs = -1, scoring = 'f1_macro')
-    # selector = RFE(estimator = DecisionTreeClassifier(), n_features_to_select = None)
     # Fit and transform the training data.
     selector.fit_transform(X = X, y = ravel(y))
     # Define the selected features.
@@ -69,12 +65,12 @@ def recursive_feature_elimination(X, y, feature_names):
 def main(X_train, y_train, X_test, feature_names):
     # Ensure the $feature_names are of type `Index` from pandas so boolean arrays can be applied.
     feature_names = Index(feature_names)
-    # Use VarianceThreshold with the default threshold to remove features from $X_train that have zero variance.
+    # Use VarianceThreshold to remove features that are stagnant or nearly constant.
     selected_features = variance_threshold(X=X_train, feature_names=feature_names)
     # Update the training and testing data to only include the $selected_features identified by variance.
     X_train, X_test = apply_selected_features(X_train, X_test, selected_features)
-    # Perform feature selection using SelectKBest based on mutual information between $X_train and $y_train.
-    selected_features = select_k_best(X=X_train, y=y_train, feature_names=X_train.columns, k=10)
+    # Perform feature selection using SelectKBest based on mutual information. k is set to 25 to allow more depth.
+    selected_features = select_k_best(X=X_train, y=y_train, feature_names=X_train.columns, k=25)
     # Update the training and testing data to only include the final $selected_features.
     X_train, X_test = apply_selected_features(X_train, X_test, selected_features)
     # Return the modified $X_train and $X_test sets along with the list of final $selected_features.
