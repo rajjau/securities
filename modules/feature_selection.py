6e98f419a70a5039b3e7c3d4655a7a35217b87b2
rajjau <elf-watch-mug@duck.com>
2025-12-28 18:53:33 -0500

    Moved the application of selected features to each function

--- modules/feature_selection.py
+++ modules/feature_selection.py
@@ -24,38 +24,44 @@ def apply_selected_features(X_train, X_test, selected_features):
     # Return the modified training and testing datasets.
     return X_train, X_test
 
-def variance_threshold(X, feature_names):
+def variance_threshold(X_train, X_test, feature_names):
     """Perform VarianceThreshold feature selection to remove features with low variance."""
     # Initialize the function. Threshold is lowered to 0.0001 to support stationary/percentage features.
-    selector = VarianceThreshold(threshold = 0.0001)
+    selector = VarianceThreshold(threshold = 0.95)
     # Fit and transform the training data.
-    selector.fit_transform(X = X)
+    selector.fit_transform(X = X_train)
     # Define the selected features.
     selected_features = feature_names[selector.get_support()]
+    # Update the training and testing data to only include the $selected_features identified by variance.
+    X_train, X_test = apply_selected_features(X_train=X_train, X_test=X_test, selected_features=selected_features)
     # Return the selected list of features.
     return selected_features
 
-def select_k_best(X, y, feature_names, k):
+def select_k_best(X_train, y_train, X_test, feature_names, k):
     """Perform SelectKBest feature selection to select features based on mutual information."""
     # Initialize the function.
     selector = SelectKBest(mutual_info_classif, k = k)
     # Fit and transform the training data.
-    selector.fit_transform(X = X, y = ravel(y))
+    selector.fit_transform(X = X_train, y = ravel(y_train))
     # Define the selected features.
     selected_features = selector.get_feature_names_out(input_features = feature_names)
     # Convert the ndarray to an Index. This keeps the output type consistent.
     selected_features = Index(selected_features)
+    # Update the training and testing data to only include the $selected_features identified by variance.
+    X_train, X_test = apply_selected_features(X_train=X_train, X_test=X_test, selected_features=selected_features)
     # Return the selected list of features.
     return selected_features
 
-def recursive_feature_elimination(X, y, feature_names):
+def recursive_feature_elimination(X_train, y_train, X_test, feature_names):
     """Perform Recursive Feature Elimination with Cross-Validation (RFECV) to select features."""
     # Initialize the function.
     selector = RFECV(estimator = DecisionTreeClassifier(), cv = TimeSeriesSplit(n_splits = 5), n_jobs = -1, scoring = 'f1_macro')
     # Fit and transform the training data.
-    selector.fit_transform(X = X, y = ravel(y))
+    selector.fit_transform(X = X_train, y = ravel(y_train))
     # Define the selected features.
     selected_features = feature_names[selector.support_]
+    # Update the training and testing data to only include the $selected_features identified by variance.
+    X_train, X_test = apply_selected_features(X_train=X_train, X_test=X_test, selected_features=selected_features)
     # Return the selected list of features.
     return selected_features
 
@@ -66,12 +72,10 @@ def main(X_train, y_train, X_test, feature_names):
     # Ensure the $feature_names are of type `Index` from pandas so boolean arrays can be applied.
     feature_names = Index(feature_names)
     # Use VarianceThreshold to remove features that are stagnant or nearly constant.
-    selected_features = variance_threshold(X=X_train, feature_names=feature_names)
-    # Update the training and testing data to only include the $selected_features identified by variance.
-    X_train, X_test = apply_selected_features(X_train, X_test, selected_features)
+    selected_features = variance_threshold(X_train=X_train, X_test=X_test, feature_names=feature_names)
     # Perform feature selection using SelectKBest based on mutual information. k is set to 25 to allow more depth.
-    selected_features = select_k_best(X=X_train, y=y_train, feature_names=X_train.columns, k=25)
-    # Update the training and testing data to only include the final $selected_features.
-    X_train, X_test = apply_selected_features(X_train, X_test, selected_features)
+    selected_features = select_k_best(X_train=X_train, y_train=y_train, X_test=X_test, feature_names=X_train.columns, k=10)
+    # Perform feature selection using RFECV.
+    # selected_features = recursive_feature_elimination(X_train, y_train, X_test, feature_names)
     # Return the modified $X_train and $X_test sets along with the list of final $selected_features.
     return X_train, X_test, selected_features
