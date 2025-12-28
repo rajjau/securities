a85f602bcb455d20063cb9c4dbc8858b59f6e32e
rajjau <elf-watch-mug@duck.com>
2025-12-28 14:44:34 -0500

    Use parquet for the cache instead of joblib

--- modules/import_and_parse.py
+++ modules/import_and_parse.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
-from joblib import dump, load
-from pandas import read_csv
+from pandas import read_parquet
 from pathlib import Path
 
 ######################
@@ -13,9 +12,9 @@ from modules.messages import msg_info, msg_error
 #################
 ### FUNCTIONS ###
 #################
-def process_data(filename, symbols, filename_joblib):
+def process_data(filename, symbols, filename_cache):
     # Read the data from $filename and import it as a DataFrame.
-    data = read_csv(filename)
+    data = read_parquet(path = filename)
     # Check if the user has defined certain symbols to train and test on.
     if symbols:
         # Message to stdout.
@@ -24,8 +23,8 @@ def process_data(filename, symbols, filename_joblib):
         data = data[data['T'].isin(symbols)]
     # Check if the data is empty after filtering by symbol(s). If so, then exit with an error message.
     if data.empty: msg_error('The data is empty after filtering by symbol(s). Please check the input file and the specified symbols in the configuration file.')
-    # Save the imported and modified data to a joblib file for faster loading in the future.
-    dump(value = data, filename = filename_joblib)
+    # Save the imported and modified data to a parquet file for faster loading in the future.
+    data.to_parquet(path = filename_cache, index = False)
     # Return the $data variable.
     return data
 
@@ -39,17 +38,17 @@ def main(filename, symbols, cache_directory, columns_x, columns_y):
     is_dir(cache_directory, exit_on_error=False)
     # Create the cache directory (if needed).
     cache_directory.mkdir(parents=True, exist_ok=True)
-    # Define a joblib filename based on the provided $filename and $symbols.
-    filename_joblib = Path(cache_directory, f"{filename.stem}_{'_'.join(symbols) if symbols else 'all'}.joblib")
-    # Check if the joblib file exists.
-    if filename_joblib.is_file():
-        # Display a message to stdout that the data will be read from the joblib file.
-        msg_info(f"Reading data from previously saved joblib file: {filename_joblib}")
-        # Read the data from the joblib file.
-        data = load(filename_joblib)
+    # Define a parquet filename based on the provided $filename and $symbols.
+    filename_cache = Path(cache_directory, f"{filename.stem}_{'_'.join(symbols) if symbols else 'all'}.parquet")
+    # Check if the parquet file exists.
+    if filename_cache.is_file():
+        # Display a message to stdout that the data will be read from the parquet file.
+        msg_info(f"Reading data from the cache file: {filename_cache}")
+        # Read the data from the cache file.
+        data = read_parquet(path = filename_cache)
     else:
-        # Import the data from the CSV file and process it. This includes filtering by $symbols and saving to a joblib file for caching.
-        data = process_data(filename=filename, symbols=symbols, filename_joblib=filename_joblib)
+        # Import the data from the parquet file and process it. This includes filtering by $symbols and saving to cache.
+        data = process_data(filename=filename, symbols=symbols, filename_cache=filename_cache)
     # If a wildcard was passed to define the feature set columns, then use all columns except for $columns_y.
     if columns_x[0] == '*': columns_x = data.columns.difference(columns_y).to_list()
     # Return the processed $data and the updated $columns_x.
