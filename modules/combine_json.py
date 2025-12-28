5aa2900803b35534a1e49e132b30abbc75a604e2
rajjau <elf-watch-mug@duck.com>
2025-12-28 14:51:23 -0500

    Use PEP 8 formatting and use ArgumentParser

--- modules/combine_json.py
+++ modules/combine_json.py
@@ -1,30 +1,32 @@
 #!/usr/bin/env python
+from argparse import ArgumentParser
 from json import load
-from pandas import concat,DataFrame
+from pandas import concat, DataFrame
 from pathlib import Path
-from sys import argv
 
 ######################
 ### CUSTOM MODULES ###
 ######################
 from modules.listify import main as listify
-from modules.messages import msg_info,msg_warn
+from modules.messages import msg_info, msg_warn
 from modules.write_csv import main as write_csv
 
 #################
 ### FUNCTIONS ###
 #################
 def args():
-    try:
-        # Argument 1: Comma separated list of files to combine.
-        filenames = argv[1]
-    except IndexError:
-        # If argument 1 was not specified, then raise an error.
-        raise IndexError('Argument 1: Comma separated list of files to combine.')
+    """Parse and return command-line arguments."""
+    # Create an ArgumentParser object.
+    parser = ArgumentParser(description='Combine the data from all JSON files into a single CSV file.')
+    # Add arguments.
+    parser.add_argument('filenames', type=str, help='Comma separated list of files to combine.')
+    parser.add_argument('filename_output', type=Path, help='Comma separated list of files to combine.')
+    # Parse the arguments.
+    args = parser.parse_args()
     # Convert the $filenames into a list from a string, and additionally, define the absolute path for every filename.
-    filenames = [Path(entry).absolute() for entry in listify(filenames)]
-    # Return the user-defined variable(s).
-    return(filenames)
+    filenames = [Path(entry).absolute() for entry in listify(args.filenames)]
+    # Return the arguments.
+    return filenames, args.filename_output.absolute()
 
 def parse(filename):
     # Display current filename.
@@ -38,7 +40,7 @@ def parse(filename):
         # If no results were found in the JSON file, then display a warning message to stdout.
         msg_warn(f"No results found for {filename}, skipping.")
         # Return bool False.
-        return(False)
+        return False
     # Convert the JSON dictionaries for each timepoint into a pandas DataFrame.
     contents = [DataFrame(entry, index = [0]) for entry in results]
     # Concatenate all DataFrames into one.
@@ -46,7 +48,7 @@ def parse(filename):
     # Add the filename as another column.
     contents['f'] = filename.stem
     # Return the $contents DataFrame.
-    return(contents)
+    return contents
 
 ############
 ### MAIN ###
@@ -57,7 +59,7 @@ def main(filenames, filename_output):
     # Iterate through each file.
     for filename in filenames:
         # Convert its JSON contents to a pandas DataFrame.
-        data = parse(filename = filename)
+        data = parse(filename=filename)
         # If the data was found, then write it to the output file. This read and write process is better for memory.
         if data is not False: write_csv(data = data, filename = filename_output)
 
@@ -66,8 +68,8 @@ def main(filenames, filename_output):
 #############
 if __name__ == '__main__':
     # Obtain user-defined arguments
-    filenames = args()
+    filenames, filename_output = args()
     # Start the script
-    data = main(filenames = filenames)
+    data = main(filenames=filenames, filename_output=filename_output)
     # Display the data to stdout.
     print(data.to_csv())
