dd6ebe6f6564009d2f1dd9033b9b3f4c77bdd8ee
rajjau <elf-watch-mug@duck.com>
2026-01-19 16:14:38 -0500

    Fixed logic for saving model based on combinations of the production flag and save threshold

--- modules/save_model.py
+++ modules/save_model.py
@@ -10,11 +10,12 @@ from modules.messages import msg_info
 ### MAIN ###
 ############
 def main(saved_model, model, score, save_threshold, is_production):
-    # Check if saving is enabled and threshold is met.
-    if (save_threshold == -1) and (is_production is False): return None
-    # Check if the $score is Nonetype, meaning the model is being trained on the entire training set, OR if it's greater than or equal to the specified save threshold, OR if the current run is a production run.
-    if (score is None) or (score >= save_threshold) or (is_production is True):
+    # Check if the $score is Nonetype, meaning the model is being trained on the entire training set, OR if it's greater than or equal to the specified save threshold.
+    score_meets_threshold = (score is None) or (score >= save_threshold)
+    # Check if either: 1) the current run is for production AND the score meets thereshold or 2) the score is not negative AND it meets the threshold.
+    if ((is_production is True) and (score_meets_threshold is True)) or ((save_threshold >= 0) and (score_meets_threshold is True)):
         # Save the model object.
         dump(model, filename = saved_model)
         # Message to stdout.
         msg_info(f"SAVED: Trained model saved to: {saved_model}")
+        
