0bd4e244241ef42c5a79cffd89f125af62e2ed1e
rajjau <elf-watch-mug@duck.com>
2026-01-19 16:27:15 -0500

    Use walk forward rolling retraining

--- modules/machine_learning_voting.py
+++ modules/machine_learning_voting.py
@@ -9,33 +9,55 @@ from functools import partial
 ######################
 from modules.calculate_results import DECIMAL_PLACES
 from modules.dynamic_module_load import main as dynamic_module_load
+from modules.machine_learning import train_predict_rolling
 from modules.messages import msg_info
 from modules.save_model import main as save_model
 from modules.save_to_filename import main as save_to_filename
+
+#################
+### FUNCTIONS ###
+#################
+def create_scoring_metric(configuration_ini):
+    # Dynamically load the scoring metric.
+    scoring_metric = dynamic_module_load(module_str=configuration_ini.get('ML', 'SCORING_METRIC'))
+    # Load extra parameters defined in the configuration.ini.
+    scoring_metric_params = loads(configuration_ini.get('ML', 'SCORING_METRIC_PARAMETERS'))
+    # Create the scorer.
+    scorer = partial(scoring_metric, **scoring_metric_params)
+    # Return the scorer.
+    return scorer
 
 ############
 ### MAIN ###
 ############
 def main(X_train, y_train, X_test, y_test, name, symbols, estimators, configuration_ini):
+    #-------------#
+    #--- Model ---#
+    #-------------#
     # Define the Voting model using unfitted models from every seed.
     model = VotingClassifier(estimators = estimators, voting = 'soft')
-    # Fit the $model to the training set.
-    model.fit(X = X_train, y = y_train)
+    #----------------#
+    #--- Training ---#
+    #----------------#
     # Check if the test set has been defined.
     if (X_test is not None) and (y_test is not None):
-        # Calculate predictions on the testing set.
-        y_preds = model.predict(X = X_test)
-        # Dynamically load the scoring metric.
-        scoring_metric = dynamic_module_load(module_str=configuration_ini.get('ML', 'SCORING_METRIC'))
-        # Load extra parameters defined in the configuration.ini.
-        scoring_metric_params = loads(configuration_ini.get('ML', 'SCORING_METRIC_PARAMETERS'))
-        # Create the scorer.
-        scorer = partial(scoring_metric, **scoring_metric_params)
-        # Calculate the score.
-        score = scorer(y_true = y_test, y_pred = y_preds)
+        # If so, then perform walk-forward rolling retraining.
+        score = train_predict_rolling(
+            model=model,
+            X_train=X_train,
+            y_train=y_train,
+            X_test=X_test,
+            y_test=y_test,
+            retrain_step_frequency=configuration_ini.getint('ML', 'RETRAIN_STEP_FREQUENCY'),
+            sliding_window_size=configuration_ini.getint('ML', 'SLIDING_WINDOW_SIZE'),
+            scoring_metric=create_scoring_metric(configuration_ini=configuration_ini)
+        )
         # Message to stdout.
         msg_info(f"VOTING CONSENSUS SCORE: {round(score * 100, DECIMAL_PLACES)}")
-        # Generate the filename for saving the model to an output file.
+    #------------#
+    #--- Save ---#
+    #------------#
+    # Generate the filename for saving the model to an output file.
     saved_model = save_to_filename(
         dir_data_saved=Path(configuration_ini.get('GENERAL', 'DATA_SAVED_DIRECTORY')).resolve(),
         name=f"Consenus_Model_{name}",
