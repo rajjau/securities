ed439bdda45c8734bf5cc98482711ea7b4c0982b
rajjau <elf-watch-mug@duck.com>
2026-01-19 18:55:07 -0500

    Added another `.strip()` and check if a given $item is blank

--- modules/convert_to_list.py
+++ modules/convert_to_list.py
@@ -5,4 +5,4 @@
 ############
 def main(string, delimiter):
     """Split a string representation of a list into an actual list based on the provided $delimiter."""
-    return [item.strip() for item in string.split(delimiter)]
+    return [item.strip() for item in string.strip().split(delimiter) if item]
