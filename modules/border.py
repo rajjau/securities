672f88b5613d6b83905d72d9dd0ee5a671bf4823
rajjau <elf-watch-mug@duck.com>
2026-01-20 20:46:07 -0500

    Added buffer setting

--- modules/border.py
+++ modules/border.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
 
+################
+### SETTINGS ###
+################
+# Number of extra characters to add to the border length.
+BUFFER = 5
+
 ############
 ### MAIN ###
 ############
 def main(message, border_char = '='):
+    # Total length of the message plus the buffer.
+    total = len(message) + BUFFER
     # Create the top and bottom borders for the $message.
-    borders = border_char * (len(message) + 5)
+    borders = border_char * total
+    # Ensure the borders match the length of the message.
+    borders = borders[:total]
     # Display the top and bottom $borders with the $message to stdout.
     print(f"\n{borders}\n{message}\n{borders}")
