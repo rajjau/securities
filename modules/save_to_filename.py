c7b92d2ae3c44dbb2ea32f5c9a61a2f102000d84
rajjau <elf-watch-mug@duck.com>
2026-01-21 16:58:06 -0500

    Changed 'symbol' to 'ticker'

--- modules/save_to_filename.py
+++ modules/save_to_filename.py
@@ -9,14 +9,14 @@ from modules.date_and_time import main as date_and_time
 ############
 ### MAIN ###
 ############
-def main(dir_data_saved, name, symbols, extension, random_state = False, timestamp = False):
+def main(dir_data_saved, name, tickers, extension, random_state = False, timestamp = False):
     """Define the filename of the output file"""
     # If not passed, then define today's date and time for the filename.
     if timestamp is False: timestamp = date_and_time(n_days_ago = 0, include_time = True).replace(' ', '_').replace(':', '')
     # Prepare the name for the filename (e.g., 'DecisionTree', 'FittedScaler', 'SelectedFeatures', etc.).
     name = name.replace(' ', '_')
-    # Build the filename using symbols.
-    filename = Path(f"{name}_{symbols}_{timestamp}.{extension}")
+    # Build the filename using tickers.
+    filename = Path(f"{name}_{tickers}_{timestamp}.{extension}")
     # If the random state was defined, add it to the filename.
     if random_state: filename = f"{filename.stem}_seed{random_state}.{extension}"
     # Use `pathlib` to return the absolute path.
